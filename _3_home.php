7bfab78012435717341df4e84a7ce9f573ac4d28
Fukigen-na-Kijitora <2301032@s.asojuku.ac.jp>
2024-12-06 15:18:37 +0900

    祝　検索機能

--- _3_home.php
+++ _3_home.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-
+ 
 if (!isset($_SESSION['user_id'])) {
     // ログインしていない場合、ログイン画面にリダイレクト
     header('Location: _2_login.php');
     exit();
 }
-
+ 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -19,73 +19,93 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 <body>
     <div class="header-img">
-        <input type="search" name="search">
+        <!-- 検索フォーム -->
+        <form method="GET" action="">
+            <input type="search" name="search" value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search'], ENT_QUOTES, 'UTF-8') : ''; ?>" placeholder="山の名前で検索">
+            <button type="submit">検索</button>
+        </form>
         <img src="./images/oasislogo.jpg" width="100" height="50">
     </div>
     <hr>
-
+ 
 <?php
-    $pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
-                    dbname=LAA1602729-oasis;charset=utf8',
-                    'LAA1602729',
-                    'oasis5');
-
-
-    //海外の山画像
-    $sql1 = "SELECT `yama_img`, `yama_name` FROM `Oasis_yama` WHERE `Region` = 1 ";
-    $result1 = $pdo->query($sql1);
-    $rows1 = $result1->fetchAll(PDO::FETCH_ASSOC);
-
-    $rowCount = count($rows1);
-
-    if($rowCount > 0){
-        echo '<h2 class="h2">海外</h2>';
-        echo '<div class="img-container-wrapper">';
-        echo '<button class="Arrow left" data-target="img-container-1">&lt;</button>';
-        echo '<div class="img-container" id="img-container-1">';
-            foreach ($rows1 as $row) {
-                echo '<div class="img-slide">';
-                echo '<form action="_4_shohin.php" method="POST">';
-                echo '<input type="hidden" name="name" value="' . htmlspecialchars($row["yama_name"], ENT_QUOTES, 'UTF-8') . '">';
-                echo '<button type="submit" class="img-transition">';
-                echo '<img src="' . $row["yama_img"] . '" alt="'. $row["yama_name"] . '">';
-                echo '</button>';
-                echo '<p>'. $row["yama_name"]. '</p>';
-                echo '</form>';
-                echo '</div>';
-            }
-        echo '</div>';
-        echo '<button class="Arrow right" data-target="img-container-1">&gt;</button>';
-        echo '</div>';
-    }
-
-    //国内の山画像
-    $sql2 = "SELECT `yama_img`, `yama_name` FROM `Oasis_yama` WHERE `Region` = 0 ";
-    $result2 = $pdo->query($sql2);
-    $rows2 = $result2->fetchAll(PDO::FETCH_ASSOC);
-
-    $rowCount = count($rows2);
-
-    if($rowCount > 0){
-        echo '<h2 class="h2">国内</h2>';
-        echo '<div class="img-container-wrapper">';
-        echo '<button class="Arrow left" data-target="img-container-2">&lt;</button>';
-        echo '<div class="img-container" id="img-container-2">';
-            foreach ($rows2 as $row) {
-                echo '<div class="img-slide">';
-                echo '<form action="_4_shohin.php" method="POST">';
-                echo '<input type="hidden" name="name" value="' . htmlspecialchars($row["yama_name"], ENT_QUOTES, 'UTF-8') . '">';
-                echo '<button type="submit" class="img-transition">';
-                echo '<img src="' . $row["yama_img"] . '" alt="'. $row["yama_name"] . '">';
-                echo '</button>';
-                echo '<p>'. $row["yama_name"]. '</p>';
-                echo '</form>';
-                echo '</div>';
-            }
-        echo '</div>';
-        echo '<div class="Arrow right" data-target="img-container-2">&gt;</div>';
+$pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
+                dbname=LAA1602729-oasis;charset=utf8',
+                'LAA1602729',
+                'oasis5');
+ 
+// 検索キーワードを取得
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+ 
+// 海外の山画像
+$sql1 = "SELECT `yama_img`, `yama_name` FROM `Oasis_yama` WHERE `Region` = 1 ";
+if ($search) {
+    $sql1 .= " AND `yama_name` LIKE :search";
+}
+$stmt1 = $pdo->prepare($sql1);
+if ($search) {
+    $stmt1->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
+}
+$stmt1->execute();
+$rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+ 
+$rowCount = count($rows1);
+ 
+if ($rowCount > 0) {
+    echo '<h2 class="h2">海外</h2>';
+    echo '<div class="img-container-wrapper">';
+    echo '<button class="Arrow left" data-target="img-container-1">&lt;</button>';
+    echo '<div class="img-container" id="img-container-1">';
+    foreach ($rows1 as $row) {
+        echo '<div class="img-slide">';
+        echo '<form action="_4_shohin.php" method="POST">';
+        echo '<input type="hidden" name="name" value="' . htmlspecialchars($row["yama_name"], ENT_QUOTES, 'UTF-8') . '">';
+        echo '<button type="submit" class="img-transition">';
+        echo '<img src="' . $row["yama_img"] . '" alt="'. $row["yama_name"] . '">';
+        echo '</button>';
+        echo '<p>' . $row["yama_name"] . '</p>';
+        echo '</form>';
         echo '</div>';
     }
+    echo '</div>';
+    echo '<button class="Arrow right" data-target="img-container-1">&gt;</button>';
+    echo '</div>';
+}
+ 
+// 国内の山画像
+$sql2 = "SELECT `yama_img`, `yama_name` FROM `Oasis_yama` WHERE `Region` = 0 ";
+if ($search) {
+    $sql2 .= " AND `yama_name` LIKE :search";
+}
+$stmt2 = $pdo->prepare($sql2);
+if ($search) {
+    $stmt2->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
+}
+$stmt2->execute();
+$rows2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+ 
+$rowCount = count($rows2);
+ 
+if ($rowCount > 0) {
+    echo '<h2 class="h2">国内</h2>';
+    echo '<div class="img-container-wrapper">';
+    echo '<button class="Arrow left" data-target="img-container-2">&lt;</button>';
+    echo '<div class="img-container" id="img-container-2">';
+    foreach ($rows2 as $row) {
+        echo '<div class="img-slide">';
+        echo '<form action="_4_shohin.php" method="POST">';
+        echo '<input type="hidden" name="name" value="' . htmlspecialchars($row["yama_name"], ENT_QUOTES, 'UTF-8') . '">';
+        echo '<button type="submit" class="img-transition">';
+        echo '<img src="' . $row["yama_img"] . '" alt="'. $row["yama_name"] . '">';
+        echo '</button>';
+        echo '<p>' . $row["yama_name"] . '</p>';
+        echo '</form>';
+        echo '</div>';
+    }
+    echo '</div>';
+    echo '<button class="Arrow right" data-target="img-container-2">&gt;</button>';
+    echo '</div>';
+}
 ?>
 <footer>
 <hr>
@@ -97,3 +117,5 @@ if (!isset($_SESSION['user_id'])) {
 <script src="./javascript/userhome.js"></script>
 </body>
 </html>
+ 
+ 
