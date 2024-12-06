787a72edaa335f35a89966f8b45f1f9d97a5e90e
2301037 <2301037@s.asojuku.ac.jp>
2024-12-06 14:46:17 +0900

    修正

--- _5_kounyu.php
+++ _5_kounyu.php
@@ -116,7 +116,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <input type="checkbox" name="kiyaku" required> 利用規約に同意する
         </div>
         <!-- 購入ボタン -->
-         <h3><input type="submit">購入する</h3>
+         <input type="submit">購入する
     </div>
 
 </form>
