92fdf3850cdac49b0ec44e81ddf44395da329354
Fukigen-na-Kijitora <2301032@s.asojuku.ac.jp>
2025-01-07 10:08:53 +0900

    削除

--- _13_kanri.shohindel.php
+++ _13_kanri.shohindel.php
@@ -35,7 +35,7 @@ $Oasis_yama = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>商品一覧</title>
+    <title>商品削除</title>
     <style>
         body {
             font-family: Arial, sans-serif;
