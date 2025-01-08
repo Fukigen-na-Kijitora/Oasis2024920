67739736c4d7c29b084d348b21a9867867142047
yo-ta0016 <2301012@s.asojuku.ac.jp>
2025-01-08 14:39:10 +0900

    うおｗ

--- _11_kanri.home.php
+++ _11_kanri.home.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/stylesheet_3.css">
     <title>ダッシュボード - 画像アップロード</title>
     <style>
         /* グローバルスタイル */
