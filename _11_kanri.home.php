068e0ff2f517ef834aa870c4ee0766a74282d16b
yo-ta0016 <2301012@s.asojuku.ac.jp>
2025-01-08 14:27:41 +0900

    uow

--- _11_kanri.home.php
+++ _11_kanri.home.php
@@ -1,4 +1,16 @@
 <?php
+$host = 'mysql306.phy.lolipop.lan';
+$dbname = 'LAA1602729-oasis';
+$user = 'LAA1602729';
+$password = 'oasis5';
+ 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "データベース接続エラー: " . $e->getMessage();
+    exit;
+}
 session_start();
 
 // セッション確認：ログイン状態でなければログイン画面へリダイレクト
